aca72ac84e75189f00484bca8228cebe8fe0c02c
Burak Önce <119293638+BurakOnce@users.noreply.github.com>
2024-02-06 00:06:38 +0300

    add findAuthoritiesByUsername

--- IEMS/src/main/java/com/example/iems/repository/UserRepository.java
+++ IEMS/src/main/java/com/example/iems/repository/UserRepository.java
@@ -1,15 +1,23 @@
 package com.example.iems.repository;
 
+import com.example.iems.model.Role;
 import com.example.iems.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String userName);
+
+    User findUsersByUsername(String Username);
+
+    @Query("SELECT u.authorities FROM User u WHERE u.username = :username")
+    List<String> findAuthoritiesByUsername(String username);
 
     List<User> findUsersByCity(String city);
 
