ba33b90ab3be78dccf1fd5a33fbd06550c298459
Burak Önce <119293638+BurakOnce@users.noreply.github.com>
2024-02-07 00:24:46 +0300

    add query for authorities

--- IEMS/src/main/java/com/example/iems/repository/UserRepository.java
+++ IEMS/src/main/java/com/example/iems/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.iems.model.Role;
 import com.example.iems.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +19,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u.authorities FROM User u WHERE u.username = :username")
     List<String> findAuthoritiesByUsername(String username);
+
+    @Query("SELECT u FROM User u JOIN u.authorities a WHERE a = :role")
+    List<User> findUserByRole(@Param("role") Role role);
 
     List<User> findUsersByCity(String city);
 
