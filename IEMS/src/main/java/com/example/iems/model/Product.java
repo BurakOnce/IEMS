41c328a5c172f400b739f9fbb6264311a5f9a965
Burak Önce <119293638+BurakOnce@users.noreply.github.com>
2024-02-09 15:19:42 +0300

    Create Product.java

---
+++ IEMS/src/main/java/com/example/iems/model/Product.java
@@ -0,0 +1,33 @@
+package com.example.iems.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@Entity
+@Table(name = "product")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+    private String description;
+    private Long stock;
+    private String barcode;
+    private Long discount;
+    private int price;
+
+    @ManyToOne
+    private Category category;
+
+}
