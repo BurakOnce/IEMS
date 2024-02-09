75f93a9e1077d6ae9a167ac537f7d9d82f7c4749
Burak Önce <119293638+BurakOnce@users.noreply.github.com>
2024-02-09 15:19:58 +0300

    Create Company.java

---
+++ IEMS/src/main/java/com/example/iems/model/Company.java
@@ -0,0 +1,35 @@
+package com.example.iems.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name = "company")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String sector;
+    private String town;
+    private String city;
+
+    @OneToOne
+    private User manager;
+
+    @OneToMany
+    private List<User> employee;
+
+
+    @OneToMany
+    private List<Product> product;
+}
