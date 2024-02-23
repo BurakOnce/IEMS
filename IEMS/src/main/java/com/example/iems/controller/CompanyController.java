66b199a261ccca2e205f1cd023f6ae6a9b1ac1d8
Burak Önce <119293638+BurakOnce@users.noreply.github.com>
2024-02-23 21:30:41 +0300

    Create CompanyController.java

---
+++ IEMS/src/main/java/com/example/iems/controller/CompanyController.java
@@ -0,0 +1,36 @@
+package com.example.iems.controller;
+
+import com.example.iems.dto.CreateCompanyRequest;
+import com.example.iems.dto.UpdateCompanyRequest;
+import com.example.iems.model.Company;
+import com.example.iems.service.CompanyService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/appCompany")
+@Slf4j
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome Burak's App";
+    }
+
+    @PostMapping("/admin/createCompany")
+    public Company createCompany(@RequestBody CreateCompanyRequest request) {
+        return companyService.createCompany(request);
+    }
+/*
+    @PostMapping("/admin/UpdateCompany")
+    public Company updateCompany(String name , @RequestBody UpdateCompanyRequest request){
+        return companyService.updateCompany(name,request);
+    }*/
+}
