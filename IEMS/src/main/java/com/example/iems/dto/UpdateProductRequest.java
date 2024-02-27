ef1cba8ddbc5c7ef83ac9e76c5092f3c8a683668
Burak Önce <119293638+BurakOnce@users.noreply.github.com>
2024-02-27 17:50:42 +0300

    Update UpdateProductRequest.java

--- IEMS/src/main/java/com/example/iems/dto/UpdateProductRequest.java
+++ IEMS/src/main/java/com/example/iems/dto/UpdateProductRequest.java
@@ -1,1 +1,25 @@
+package com.example.iems.dto;
+
+import com.example.iems.model.Product;
+import com.example.iems.model.User;
+import jakarta.persistence.Column;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Builder
+@Getter
+@Setter
+public class UpdateProductRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private Long stock;
+    private String barcode;
+    private Long discount;
+    private Float price;
+}
 
