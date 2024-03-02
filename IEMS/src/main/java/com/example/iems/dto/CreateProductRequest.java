b8292dcdaeb9f8d7a4629561d1e193e1169f71b5
Burak Önce <119293638+BurakOnce@users.noreply.github.com>
2024-03-03 00:14:57 +0300

    Update CreateProductRequest.java

--- IEMS/src/main/java/com/example/iems/dto/CreateProductRequest.java
+++ IEMS/src/main/java/com/example/iems/dto/CreateProductRequest.java
@@ -1,19 +1,24 @@
 package com.example.iems.dto;
 
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateProductRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private Long stock;
+    private String barcode;
+    private Long discount;
 
-@Builder
-public record CreateProductRequest(
+    private Float price;
 
-        Long id,
-        String name,
-        String description,
-        Long stock,
-        String barcode,
-        Long discount,
-        Float price
-){
 }
+
