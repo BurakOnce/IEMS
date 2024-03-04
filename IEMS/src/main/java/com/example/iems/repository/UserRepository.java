36d12bbbc9bcfbe36ba001ae2f7e83ed023d676b
Burak Önce <119293638+BurakOnce@users.noreply.github.com>
2024-03-04 21:27:27 +0300

    Update UserRepository.java

--- IEMS/src/main/java/com/example/iems/repository/UserRepository.java
+++ IEMS/src/main/java/com/example/iems/repository/UserRepository.java
@@ -5,23 +5,23 @@ import com.example.iems.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String userName);
-
-    User findUsersByUsername(String Username);
+    User findByUsername(String Username);
+    Optional<User> findUsersByUsername(String userName);
 
     @Query("SELECT u.authorities FROM User u WHERE u.username = :username")
     List<String> findAuthoritiesByUsername(String username);
 
     @Query("SELECT u FROM User u JOIN u.authorities a WHERE a = :role")
-    List<User> findUserByRole(@Param("role") Role role);
+    Optional<User> findUserByRole(@Param("role") Role role);
 
     List<User> findUsersByCity(String city);
 
